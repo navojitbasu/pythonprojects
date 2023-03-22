e0c73bb545b4e5e027cf6a372b5a7a474ea7f912
navojitbasu <49829240+navojitbasu@users.noreply.github.com>
2023-03-22 07:18:58 +0530

    Add files via upload

---
+++ wordreplacement.py
@@ -0,0 +1,7 @@
+def replace_word():
+    str = "Hi everyone, i am Nav, I am learning Python"
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the word replacement: ")
+    print(str.replace(word_to_replace, word_replacement))
+
+replace_word()
